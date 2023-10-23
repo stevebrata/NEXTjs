6f74a8f483966daa82e0dcb74de0e4f8f1b1444d
stevebrata <stevebrata009@gmail.com>
2023-10-23 23:23:01 +0800

    delapan

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
-"use client"
-
-import { useSession } from "next-auth/react"
 import MovieList from "./components/MovieList"
-import { useRouter } from "next/navigation"
 
 export default function Page() {
-  const { status } = useSession()
-  const router = useRouter()
+
 
   return (
     < MovieList />
