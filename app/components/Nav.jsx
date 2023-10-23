4e348856d86cd380a9eedbc5cf3518ac31acc3de
stevebrata <stevebrata009@gmail.com>
2023-10-23 21:53:51 +0800

    delapan

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -38,10 +38,13 @@ export default function Nav() {
         <input value={name} onChange={(e) => setName(e.target.value)} onKeyDown={handleSubmit} className=" block rounded-md px-4 py-2 mx-auto" placeholder="search"></input>
       </div>
       <div>
+        {status === "unauthenticated" ? (
 
-        <button onClick={signOut}>sign out</button>
-        <p>sign in as {data?.user?.name}</p>
-
+          ""
+        ) : <>
+          <button onClick={signOut}>sign out</button>
+          <p>sign in as {data?.user?.name}</p></>
+        }
       </div>
     </div >
   )
--- app/page.js
+++ app/page.js
@@ -2,19 +2,21 @@
 import Login from "./components/Login"
 import { useSession } from "next-auth/react"
 import MovieList from "./components/MovieList"
-
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Page() {
   const { status } = useSession()
+  const router = -useRouter()
 
-
+  useEffect(() => {
+    router.push('/')
+  }, status)
   return (
     <>{status === "unauthenticated" ?
       <Login />
       :
-
       < MovieList />
-
     }
     </>
   )
