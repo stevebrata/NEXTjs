79b61be3b8eee7e4d995ba97c248530bdf62ac55
stevebrata <stevebrata009@gmail.com>
2023-10-25 18:01:36 +0800

    delapan

--- app/components/util/Pagination.jsx
+++ app/components/util/Pagination.jsx
@@ -1,14 +1,14 @@
 "use client"
 
 export default function Pagination({ page, setPage, movies }) {
-  console.log(movies)
+  const nextPage = page + 1
   return (
     <div className="flex justify-center gap-2 text-xl font-bold">
       {page <= 1 ? null :
         <button onClick={() => setPage((prev) => prev - 1)}>prev</button>
       }
       <span>{page}</span>
-      {movies.Error ? null :
+      {nextPage && movies.Error ? null :
         <button onClick={() => setPage((prev) => prev + 1)}>next</button>
       }
     </div>
